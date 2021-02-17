f852c8a8f6c22e4ce61a77854b592fd40191955c
Michał Kowala <michalkowala111@gmail.com>
2021-02-17 11:18:25 +0100

    Add HttpClient to call poe api

---
+++ tradeofexile.infrastructure/ApiHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace tradeofexile.infrastructure
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public static string LoadPoeApi()
+        {
+            string url = "http://api.pathofexile.com/public-stash-tabs";
+            HttpResponseMessage response = ApiHelper.ApiClient.GetAsync(url).Result;
+            var responsejson = response.Content.ReadAsStringAsync();
+            return responsejson.Result;
+        }
+    }
+}
