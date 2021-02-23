0a73fb449c93d8d0efe383a03b67dc83a77a4f27
Michał Kowala <michalkowala111@gmail.com>
2021-02-23 13:38:02 +0100

    Add method returning list of unique items by item class

---
+++ tradeofexile.infrastructure/ItemExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using tradeofexile.models.Enums;
+
+namespace tradeofexile.infrastructure
+{
+    public static class ItemExtensions
+    {
+        public static List<string> GetListOfUniqueItemsByClass(ItemClass itemClass)
+        {
+            string gamepediaCallUrl = ApiItemQuerier.GetItemAndRarityParametriziedGamepediaCallUrl(itemClass, ItemRarity.Unique, ResponseFormat.json);
+            string response = ApiHelper.GetResponseFromApi(gamepediaCallUrl+"&format=json");
+            List<string> listOfItems = new List<string>();
+            JObject jObject = JObject.Parse(response);
+            foreach (JToken t in jObject["cargoquery"])
+            {
+                JToken token = t.Value<JToken>("title");
+                listOfItems.Add(token.Value<string>("name"));
+            }
+            return listOfItems;
+        }
+    }
+}
