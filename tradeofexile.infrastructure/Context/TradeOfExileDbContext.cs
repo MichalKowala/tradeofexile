4b63407ea7ab131ab3f487804a9472640f9511ea
Michał Kowala <michalkowala111@gmail.com>
2021-03-05 03:13:17 +0100

    Add dbcontext

---
+++ tradeofexile.infrastructure/Context/TradeOfExileDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tradeofexile.models.Items;
+
+namespace tradeofexile.infrastructure.Context
+{
+    public class TradeOfExileDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=tradeofexiledb;user=root;pwd=HasloMaslo");
+        }
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasKey(x => x.Id);            
+        }
+    }
+}
