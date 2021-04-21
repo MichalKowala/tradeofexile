182e5db8d38d55ce999feb7f81cb1db166be7992
Michał Kowala <michalkowala111@gmail.com>
2021-04-21 16:02:00 +0200

    Work on styles

---
+++ tradeofexile/Controllers/ItemsController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tradeofexile.Controllers
+{
+    public abstract class ItemsController : Controller
+    {
+        private readonly IMediator _mediator;
+        public ItemsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
